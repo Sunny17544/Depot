5bb21914bb1bad3a66c13d83e1aa8693e6c704dc
Sandeep Roy <sandeep@greycampus.com>
2021-06-29 04:03:38 +0530

    Add: Stimulus Functionality and its Functions

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -9,24 +9,32 @@
 
 import { Controller } from "stimulus"
 
-export default class extends Controller {
-  static targets = [ "output", "detail", "cart" ]
-  static values = { url: String , count: Number}
+export default class extends Controller 
+{
+  static targets = [ "Description","Details", "Cart" ]
+  static values = {  count: Number, url: String  }
 
-  connect() {
-    this.outputTarget.textContent = 'Hello, Stimulus!'
-  }
-
-  greet()
+  connect() 
   {
-    // var nameStr=1;
-    // alert(nameStr)
-    // console.log("Sandeep Roy")
-    alert(this.detailTarget.textContent) 
+    //this.outputTarget.textContent = 'Hello, Stimulus!'
   }
 
-
-
+  Description()
+  {
+    console.log("Description Working")
+    // alert(nameStr)
+    console.log(this.application)
+    console.log(this.element)
+    var details=this.DetailsTargets;
+    var id1 = " ";
+    // var nameStr=1;
+    for (var id= 0, len= details.length; id<len; id++)
+    {
+      id1 += details[id].textContent;
+    }   
+    var id2=this.DescriptionTarget.textContent;
+    alert(id1+id2);
+  }
 
 
 CartAdd(event)
@@ -34,7 +42,7 @@ CartAdd(event)
   console.log("Working")
   const csrf = document.querySelector("[name='csrf-token']").getAttribute("content");
   var product_id= event.target.id;
-  const data={product_id: product_id , quantity: 1}
+  const data={product_id: product_id}
   var url=this.urlValue;
 
   fetch(url,
@@ -52,7 +60,7 @@ CartAdd(event)
     if (response.status == 200 ) 
     { 
       this.countValue=this.countValue+1 ; 
-      this.cartTarget.textContent=this.countValue;
+      this.CartTarget.textContent=this.countValue;
     } 
     else if(response.status == 404) 
     { 
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -12,8 +12,23 @@ class Cart < ApplicationRecord
             end
         current_item
     end
+
+    def remove_product(product)
+        current_item = line_items.find_by(product_id: product.id)
+        if current_item.quantity > 1
+            current_item.quantity -= 1
+        else
+          #current_item.quantity ||= 1
+        end
+        current_item
+    end
     
     def total_price
         line_items.to_a.sum { |item| item.total_price }
     end
+
+    def self.quantity_count(id)
+        (id) ? Cart.find(id).line_items.sum { |x| x['quantity']} : 0
+    end
+    
 end
