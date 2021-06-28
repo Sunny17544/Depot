98840f72593a47e286201a1115eebc7490e6024a
Sandeep Roy <sandeep@greycampus.com>
2021-06-28 13:57:01 +0530

    Add: shops page and Nav-bar

---
+++ app/controllers/shop_controller.rb
@@ -0,0 +1,7 @@
+class ShopController < ApplicationController
+
+  def index
+    @products = Product.order(:title)
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  
   resources :products
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'shop#index', as: 'shop_index'
+  
 end
